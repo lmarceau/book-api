0f4c982a1a564f05b4cfabb046723af0035406b9
Laurie Marceau <l.marceau@mphase.ca>
2019-11-04 14:44:39 -0500

    Implementing bookRouter

--- app.js
+++ app.js
@@ -17,45 +17,11 @@ mongoose.connect(connStr, options, function(err) {
 const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = express.Router();
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
-// Books route
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre
-    }
-    Book.find(query, (err, books) => {
-      if(err) {
-        return res.send(err);
-      }
-      return res.json(books); 
-    });
-  });
-
-// Route with book identifier
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-
-    Book.findById(req.params.bookId, (err, book) => {
-      if(err) {
-        return res.send(err);
-      }
-      return res.json(book); 
-    });
-  });
-
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
