e9eca798fcfc8ae7c6da4f360e613f47c5ab1f8c
Laurie Marceau <l.marceau@mphase.ca>
2019-11-04 16:52:14 -0500

    Format

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const mongoose = require('mongoose');
 // Database setup
 var connStr = 'mongodb+srv://nodejs-db-iacsh.mongodb.net/NodeJS?retryWrites=true&w=majority';
 var options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    user: '',
-    pass: ''
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: '',
+  pass: ''
 };
-mongoose.connect(connStr, options, function(err) {
+mongoose.connect(connStr, options, function (err) {
   if (err) throw err;
 });
 
