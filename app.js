51bad79d788dcf0ebbbf19f38038772df83308c6
Laurie Marceau <l.marceau@mphase.ca>
2019-11-04 19:11:17 -0500

    Added an integration test

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 // TODO: Environment variables for user and pass
-// Database setup
-var connStr = 'mongodb+srv://nodejs-db-iacsh.mongodb.net/NodeJS?retryWrites=true&w=majority';
+// Connection options
 var options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   user: '',
   pass: ''
 };
+
+// Setup connection string
+if (process.env.ENV === 'Test') {
+  var connStr = 'mongodb+srv://npmnodejs-db-iacsh.mongodb.net/NodeJS-Test?retryWrites=true&w=majority';
+} else {
+  var connStr = 'mongodb+srv://nodejs-db-iacsh.mongodb.net/NodeJS?retryWrites=true&w=majority';
+}
+
+// Connect to database
 mongoose.connect(connStr, options, function (err) {
   if (err) throw err;
 });
 
-const bodyParser = require('body-parser')
-const app = express();
-const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
+
+const port = process.env.PORT || 3000;
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -28,6 +37,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to my Nodemon API!');
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+module.exports = app;
