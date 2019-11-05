e09b76bac16eeba047d73a242f48ef6d3c1b2fbd
Laurie Marceau <l.marceau@mphase.ca>
2019-11-04 19:27:37 -0500

    Added HATEOAS

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -26,7 +26,13 @@ function routes(Book) {
 
   // Route with book identifier
   bookRouter.route('/books/:bookId')
-    .get((req, res) => res.json(req.book))
+    .get((req, res) => {
+      const returnBook = req.book.toJSON();
+      returnBook.links = {};
+      const genre = req.book.genre.replace(' ', '%20')
+      returnBook.links.FilterByThisGenre = `http://${req.headers.host}/api/books/?genre=${genre}`;
+      res.json(returnBook);
+    })
     .put((req, res) => {
       const { book } = req;
       book.title = req.body.title;
